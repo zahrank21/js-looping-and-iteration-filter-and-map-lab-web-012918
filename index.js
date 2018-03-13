3c79691e0e81da584e49c4f623f23e1c3adcf3ee
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-13 09:57:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, string){
+  return drivers.filter (function (driver) {return driver.revenue > string});
+}
+
+function driverNamesWithRevenueOver(drivers, string){
+  let selectedDrivers = driversWithRevenueOver(drivers, string);
+  return selectedDrivers.map(function (driver) {return driver.name});
+}
+
+function exactMatch(drivers, attrs){
+  console.log(drivers[0])
+  for (const prop in attrs){
+    return drivers.filter (function (driver)  { return driver[prop] == attrs[prop] } ) ;
+  }
+}
+
+function exactMatchToList(drivers, attrs){
+
+  let selectedDrivers = exactMatch(drivers, attrs)
+  return selectedDrivers.map(function(driver) {return driver.name});
+}
